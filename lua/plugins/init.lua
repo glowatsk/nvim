056121d6a85a8986605423d20e43f99d0380c08b
Brandon Glowatski <glowatsk@gmail.com>
2023-02-13 11:51:59 -0700

    Remove flit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,12 @@
 return {
-    { "tpope/vim-fugitive" },
-    { "tpope/vim-surround" },
+	{ "tpope/vim-fugitive" },
+	{ "tpope/vim-surround" },
 	{ "tpope/vim-repeat", event = "VeryLazy" },
-    { "norcalli/nvim-colorizer.lua" },
-    { "windwp/nvim-autopairs", config = true }, -- See `config` under https://github.com/folke/lazy.nvim#-plugin-spec
-    { "numToStr/Comment.nvim", config = true },
-    { "williamboman/mason-lspconfig.nvim" },
+	{ "norcalli/nvim-colorizer.lua" },
+	{ "windwp/nvim-autopairs", config = true }, -- See `config` under https://github.com/folke/lazy.nvim#-plugin-spec
+	{ "numToStr/Comment.nvim", config = true },
+	{ "williamboman/mason-lspconfig.nvim" },
 	{ "nvim-lua/plenary.nvim", lazy = true },
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 	{ "MunifTanjim/nui.nvim", lazy = true },
-	{ "ggandor/flit.nvim", opts = { labeled_modes = "nv" } },
 }
